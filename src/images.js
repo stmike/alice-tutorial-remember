dfed182c85eee88b864fb74c84fa2dd8d7560581
Michael Zakharov <mzself@yandex.com>
2019-12-24 18:44:57 +0300

    Create images.js

---
+++ src/images.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // Идентификаторы изображений навыка (вставить актуальные значения свойств):
+  imgDefault: 'your-img-id-1',
+  imgRemember: 'your-img-id-2',
+  imgForget: 'your-img-id-3',
+};
